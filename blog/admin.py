85632bfb253d714cdaff136bde486a4f5f8505e9
unknown <danielvitor_14@outlook.com>
2020-10-24 15:55:10 -0300

    criação do modelo Post

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)#?
+class PostAdmin(admin.ModelAdmin): #?
+    list_display   = ('titulo','autor','publicado','status') # Mostrar os campos destacados.
+
+    list_filter    = ('status','criado','publicado','autor') # Lista de filtros para campos predefinidos.
+    date_hierarchy = 'publicado' # Pesquisar por data os 'publlicados'
+    raw_id_fields  = ('autor',) # Campo para adicionar o autor através de pesquisa. 
+    search_fields  = ('titulo','conteudo')   # Colocar barra de pesquisa e filtra oq se pede.
+
+    prepopulated_fields = {'slug':('titulo',)}  # Deixar o preenchimento do campo automatico através de outro campo.
 
 
 
--- blog/models.py
+++ blog/models.py
@@ -2,27 +2,32 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-class Post(models.Model):
+class Post(models.Model):#?
+    # Variavel com opções de 'status'.
     STATUS = (
         ('rascunho','Rascunho'),
-        ('publicado','Publicado'),
-    )
-    titulo = models.CharField(max_length=250)
-    slug   = models.SlugField(max_length=250)
-    autor  = models.ForeignKey(User,
-                                on_delete=models.CASCADE)
-    
-    conteudo  = models.TextField()
-    publicado = models.DateTimeField(default=timezone.now)
-    criado    = models.DateTimeField(auto_now_add=True)
-    alterado  = models.DateTimeField(auto_now=True)
-    status    = models.CharField(max_length=10,
-                                    choices=STATUS,
-                                    default='rascunho')
-    class Meta:
-        ordering = ('publicado',)
+        ('publicado','Publicado'),  
+    ) 
 
-    def __str__(self):
+    titulo = models.CharField(max_length=250) # Acrescentar o campo de preenchimento de texto "normal" com limitação de caractere.
+    slug   = models.SlugField(max_length=250) # Acrescentar o campo de preenchimento de texto separado por caractere especifico.
+    autor  = models.ForeignKey(User,
+                                on_delete=models.CASCADE) #?
+    
+    conteudo  = models.TextField() # Campo de texto
+
+    publicado = models.DateTimeField(default=timezone.now) #? 
+    criado    = models.DateTimeField(auto_now_add=True) #?
+    alterado  = models.DateTimeField(auto_now=True) #?
+
+    status    = models.CharField(max_length=15,
+                                    choices=STATUS,
+                                    default='rascunho') # Definir o campo status apartir das opções da variavel 'STATUS'.
+
+    class Meta:
+        ordering = ('-publicado',) #O rdenar pelo conteudo do campo status.
+
+    def __str__(self): #?
         return self.titulo
 
 
