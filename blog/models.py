5441f174e61351d05190a98feb29a10abcda11be
unknown <danielvitor_14@outlook.com>
2020-11-27 12:14:24 -0300

    Create,detail e updateview

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
-class Post(models.Model):#?
+class publishedManager(models.Manager):
+    '''def get_queryset(self):
+        return super(publishedManager,self).get_queryset()\
+                                           .filter(status='publicado')'''
+
+
+class Post(models.Model):# Classe com os posts do django
     # Variavel com opções de 'status'.
     STATUS = (
         ('rascunho','Rascunho'),
-        ('publicado','Publicado'),  
+        ('publicado','Publicado'),
+		
     ) 
-
+	
     titulo = models.CharField(max_length=250) # Acrescentar o campo de preenchimento de texto "normal" com limitação de caractere.
     slug   = models.SlugField(max_length=250) # Acrescentar o campo de preenchimento de texto separado por caractere especifico.
     autor  = models.ForeignKey(User,
@@ -16,16 +24,21 @@ class Post(models.Model):#?
     
     conteudo  = models.TextField() # Campo de texto
 
-    publicado = models.DateTimeField(default=timezone.now) #? 
-    criado    = models.DateTimeField(auto_now_add=True) #?
-    alterado  = models.DateTimeField(auto_now=True) #?
+    publicado = models.DateTimeField(default=timezone.now)
+    criado    = models.DateTimeField(auto_now_add=True)
+    alterado  = models.DateTimeField(auto_now=True)
 
     status    = models.CharField(max_length=15,
                                     choices=STATUS,
                                     default='rascunho') # Definir o campo status apartir das opções da variavel 'STATUS'.
 
+    published = publishedManager()
+
+	#def get_absolute_url(self):
+	#	return reverse('post_detail',args=[self.pk])
+
     class Meta:
-        ordering = ('-publicado',) #O rdenar pelo conteudo do campo status.
+        ordering = ('publicado',) # Ordenar pelo conteudo do campo status.
 
     def __str__(self): #?
         return self.titulo
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView, UpdateView
+from django.views.generic.edit import CreateView
+from . models import Post
 # Create your views here.
+
+class BlogListView(ListView): # Listagem dos posts
+    model = Post
+    template_name = 'blog/home.html'
+
+class BlogDetailView(DetailView): # Detalhamento dos posts
+	model = Post
+	template_name = 'blog/post_detail.html'
+	#context_object_name = 'custom'
+	
+class BlogCreateView(CreateView): # Criação dos posts
+	model = Post
+	template_name = 'blog/post_new.html'
+	fields = 'titulo','autor','conteudo','status'
+	
+class BlogUpdateView(UpdateView): # Edição dos posts
+	model = Post
+	template_name = 'blog/post_edit.html'
+	fields = 'titulo','conteudo'
+	success_url = 'blog/post_detail.html'
